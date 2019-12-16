56d97febdbd3ad7defd4ea4f656b69065e015b90
Alex <boguslavets@gmail.com>
2019-12-16 12:57:48 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ Category.destroy_all
+ categories = Category.create([{ title: 'Book' }, { title: 'Magazine' }, { title: 'Manga' }])
+
+ Product.destroy_all
+
+ 10.times do |x|
+   Product.create(
+       title: "Book title #{ x }",
+       description: "Books descr #{ x }",
+       price: rand(1..100),
+       category_id: 1 )
+ end
+
+ 10.times do |x|
+   Product.create(
+       title: "Magazine title #{ x }",
+       description: "Magazine descr #{ x }",
+       price: rand(1..77),
+       category_id: 2 )
+ end
+
+ 10.times do |x|
+   Product.create(
+       title: "Manga title #{ x }",
+       description: "Manga descr #{ x }",
+       price: rand(1..20),
+       category_id: 3 )
+ end
+
+
+ p "Created #{Category.count} categories entries."
+ p "Created #{Product.count} products entries."
