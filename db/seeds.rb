3c592c4bcd87f7a1535343bb857990991bef89bb
Alex <boguslavets@gmail.com>
2019-12-16 17:29:55 +0200

    views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
- Category.destroy_all
- categories = Category.create([{ title: 'Book' }, { title: 'Magazine' }, { title: 'Manga' }])
+#Category.destroy_all
+categories = Category.create([{ title: 'Book' }, { title: 'Magazine' }, { title: 'Manga' }])
 
- Product.destroy_all
+#Product.destroy_all
 
  10.times do |x|
    Product.create(
-       title: "Book title #{ x }",
-       description: "Books descr #{ x }",
+       title: "Book title #{ +x }",
+       description: "Books descr #{ +x }",
        price: rand(1..100),
        category_id: 1 )
  end
 
  10.times do |x|
    Product.create(
-       title: "Magazine title #{ x }",
-       description: "Magazine descr #{ x }",
+       title: "Magazine title #{ +x }",
+       description: "Magazine descr #{ +x }",
        price: rand(1..77),
        category_id: 2 )
  end
 
  10.times do |x|
    Product.create(
-       title: "Manga title #{ x }",
-       description: "Manga descr #{ x }",
+       title: "Manga title #{ +x }",
+       description: "Manga descr #{ +x }",
        price: rand(1..20),
        category_id: 3 )
  end
