68cdff89cce2582d7ebee3face08a5c3e59631e5
Alex <boguslavets@gmail.com>
2019-12-20 10:30:25 +0200

    Installed Bootstrap with Webpack with Rails 6

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
   root to: 'products#index'
   resources :products
+#  scope :admin as: :admin do
+#  resources :categories, controller:
+#      resources :products, controller:
 end
+#end
