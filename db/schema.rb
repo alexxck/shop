99fcd8f1260ee6c0aa69c57c283cd6a47e3c3014
Alex <boguslavets@gmail.com>
2019-12-25 18:40:49 +0200

    add images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_091709) do
+ActiveRecord::Schema.define(version: 2019_12_24_125408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_091709) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
+    t.string "images", default: [], array: true
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
