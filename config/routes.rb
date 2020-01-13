e0a035df76fa3d52f40dc4c69cc6f6ad54861f4e
Alex <boguslavets@gmail.com>
2020-01-13 15:14:46 +0200

    carousel

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
   root to: 'products#index'
   resources :products do
-    resources :images, :only => [:create, :destroy]
+    resources :images
   end
   resources :categories
   resources :users
