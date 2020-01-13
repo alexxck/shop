61f85a5132c34e6f82b5e1b727a3ed35959307fc
Alex <boguslavets@gmail.com>
2020-01-13 09:16:10 +0200

    add images backoffice products & categories views

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@
 class Product < ApplicationRecord
   mount_uploaders :images, ProductsImageUploader
   belongs_to :category
+  validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
   root to: 'products#index'
-  resources :products
+  resources :products do
+    resources :images, :only => [:create, :destroy]
+  end
+  resources :categories
   resources :users
   resources :sessions
 
