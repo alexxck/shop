105139fbc25bb7c8c7ef90ecc3179f92f4b58a86
Alex <boguslavets@gmail.com>
2020-01-14 13:38:37 +0200

    try sort products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,22 @@ class ProductsController < ApplicationController
   before_action :set_product, only: %i[show]
 
   def index
-    @products = Product.order(:created_at).page(params[:page])
+    # @products = Product.order(:created_at).page(params[:page])
+
+    @products = if params[:search]
+                  Product.search(params[:search]).call(page: params[:page])
+                elsif params[:sort] == 'cheapest'
+                  Product.all.cheapest.call(page: params[:page])
+                elsif params[:sort] == 'expensive'
+                  Product.all.expensive.call(page: params[:page])
+                elsif params[:sort] == 'newest'
+                  Product.all.expensive.call(page: params[:page])
+                elsif params[:sort] == 'oldest'
+                  Product.all.expensive.call(page: params[:page])
+                else
+                  Product.order(:created_at).page(params[:page])
+                end
+    @random_product = @products.sample
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,9 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :description, presence: true
   has_rich_text :description
+
+  scope :cheapest, -> { order("price ASC") }
+  scope :expensive, -> { order("price DESC") }
+  scope :newest, -> { order("created_at DESC") }
+  scope :oldest, -> { order("created_at ASC") }
 end
